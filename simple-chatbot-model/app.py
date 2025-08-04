99a99fb42fe3b00c2fd770d27e74710a200e17fa
AnishRN <anish.17175@sakec.ac.in>
2025-08-04 21:18:21 +0530

    Update app.py

--- simple-chatbot-model/app.py
+++ simple-chatbot-model/app.py
@@ -5,6 +5,8 @@ from langchain_ollama import OllamaLLM
 import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
 
 load_dotenv()
 
@@ -12,7 +14,7 @@ load_dotenv()
 os.environ['HUGGINGFACE_API_KEY'] = st.secrets["HF_TOKEN"]
 
 # Create embeddings
-from langchain_huggingface import HuggingFaceEmbeddings
+
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 ##Prompt Template
@@ -28,7 +30,7 @@ st.title("Simple Chatbot with Ollama")
 input_text = st.text_input("Ask a question:")
 
 ##Model Initialization
-llm = OllamaLLM(model="gemma:2b")
+llm = ChatGroq(groq_api_key=st.secrets["GROQ_API_KEY"], model="Gemma2-9B-it")
 output_parser = StrOutputParser()
 chain = prompt | llm | output_parser
 
@@ -42,3 +44,4 @@ if input_text:
             
 
             
+
