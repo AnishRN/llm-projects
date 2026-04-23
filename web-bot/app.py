2b9adee818ce91dd88a4ef306d192a129e9145b6
AnishRN <anish.17175@sakec.ac.in>
2026-04-23 10:28:35 +0530

    Update app.py

--- web-bot/app.py
+++ web-bot/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import validators
 import re
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 from langchain.chains.summarize import load_summarize_chain
 from langchain_community.document_loaders import YoutubeLoader, UnstructuredURLLoader
